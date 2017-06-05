fc0f7273499bd1fb1ab24b874cf7e5415369fe34
tlv <1516446184@qq.com>
2017-06-05 12:46:33 +0800

    congig init

---
+++ src/main/java/com/echo/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package com.echo.dao;
+
+import com.echo.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
